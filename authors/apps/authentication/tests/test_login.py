bbd338af91555b74157f896d85ebef8e7c38a149
dnuwa <39625835+dnuwa@users.noreply.github.com>
2018-11-01 17:08:12 +0300

    feature(descriptive signup error messages): Users should receive descriptive signup/Registration validation errors (#10)
    - existing user email validation
    - invalid email validation
    - password of length less than 8 validation
    - add password not being alphanumeric validation
    - add tests for registering user validations
    - add tests for creating user models
    [Fixes #161348792]

--- authors/apps/authentication/tests/test_login.py
+++ authors/apps/authentication/tests/test_login.py
@@ -10,6 +10,7 @@ User = get_user_model()
 signup_url = reverse("authentication:signup")
 login_url = reverse("authentication:login")
 
+
 class UserApiTestCase(APITestCase):
     def setUp(self):
 
@@ -26,23 +27,27 @@ class UserApiTestCase(APITestCase):
                 "password": "testpassword#123",
             }
         }
-    
+
     def test_login_user(self):
         # Register User
-        self.response = self.client.post(signup_url, self.signup_data, format="json")
+        self.response = self.client.post(
+            signup_url, self.signup_data, format="json")
 
         #  Verify user account
         user = User.objects.get(email=self.login_data["user"]["email"])
         uid = force_text(urlsafe_base64_encode(user.email.encode("utf8")))
         activation_token = default_token_generator.make_token(user)
-        url = reverse("authentication:activate_account", args=(uid, activation_token,))
+        url = reverse("authentication:activate_account",
+                      args=(uid, activation_token,))
         self.client.get(url, format="json")
 
         # Test user login
-        self.login_response = self.client.post(login_url, self.login_data, format="json")
+        self.login_response = self.client.post(
+            login_url, self.login_data, format="json")
         self.assertEqual(self.login_response.status_code, status.HTTP_200_OK)
         login_token = self.login_response.data['token']
         self.assertEqual(
-            self.login_response.data, {"email": "judeinno@test.com", "username": "judeinno",  
-            "token" : login_token}
+            self.login_response.data, {"email": "judeinno@test.com", "username": "judeinno",
+                                       "token": login_token}
         )
+
